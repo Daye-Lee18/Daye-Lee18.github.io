c99bf0ba554b9a25fd10162bd33c9d566625cf10
Daye-Lee18 <102942350+Daye-Lee18@users.noreply.github.com>
2025-08-21 13:05:17 +0000

    Deploying to gh-pages from @ Daye-Lee18/Daye-Lee18.github.io@28790dd20aac534189aa83043f531ed17f7e3a46 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -451,23 +451,23 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-healthgenie-personalized-health-advisor",
+          title: 'HealthGenie, Personalized Health Advisor',
+          description: "A personalized weight management service based on a data-drived approach",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+            },},{id: "projects-legal-dialogue-inpainter",
+          title: 'Legal Dialogue Inpainter',
+          description: "Conversational Question Answering Systems for SNU laws",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
+            },},{id: "projects-personalization-via-few-shot-learning",
+          title: 'Personalization via Few-shot Learning',
           description: "a project that redirects to another website",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
+            },},{id: "projects-humogen-x",
+          title: 'HuMoGen-X',
           description: "another without an image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
@@ -481,16 +481,11 @@ ninja.data = [{
           description: "a project with no image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
+            },},{id: "projects-emotion-to-video-retrieval",
+          title: 'Emotion-to-Video Retrieval',
           description: "with background image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
             },},{id: "projects-project-9",
           title: 'project 9',
           description: "another project with an image 🎉",
