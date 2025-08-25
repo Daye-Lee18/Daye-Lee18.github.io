9a93f7dfbe747dee8f0628ce7d633c97ed5a232e
Daye-Lee18 <102942350+Daye-Lee18@users.noreply.github.com>
2025-08-25 13:44:31 +0000

    Deploying to gh-pages from @ Daye-Lee18/Daye-Lee18.github.io@ce7866ef593988ad187fad79e8120684ab7c7791 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -4,7 +4,7 @@ const ninja = document.querySelector('ninja-keys');
 // add the home and posts menu items
 ninja.data = [{
     id: "nav-about",
-    title: "about",
+    title: "About",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
@@ -29,6 +29,13 @@ ninja.data = [{
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
+          },
+        },{id: "nav-teaching",
+          title: "Teaching",
+          description: "classes, workshops, and teaching material",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/teaching/";
           },
         },{id: "post-a-post-with-plotly-js",
         
