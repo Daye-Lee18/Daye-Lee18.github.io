d181573fb50f4c8556a1fd272e3821e73415e709
Daye-Lee18 <102942350+Daye-Lee18@users.noreply.github.com>
2025-09-25 15:53:36 +0000

    Deploying to gh-pages from @ Daye-Lee18/Daye-Lee18.github.io@97983a991cad5f59bcaf30be638108a9cbcf2762 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -441,6 +441,11 @@ ninja.data = [{
           description: "A personalized weight management service based on a data-drived approach",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
+            },},{id: "projects-explainable-video-qa",
+          title: 'Explainable Video-QA',
+          description: "Explainable Video-QA / VideoRAG based on HyperCLOVA X / SEED (Vision-Instruct)",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/5_project/";
             },},{id: "projects-dance",
           title: 'Dance',
           description: "Amateur Dancer at SNU",
